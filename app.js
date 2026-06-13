d72cd383a247fb7ab931dece49596ac4e8218856
jetseo <81826376+jetseo@users.noreply.github.com>
2026-06-13 17:08:53 +0900

    Swap share/save button order + spacing

--- app.js
+++ app.js
@@ -480,8 +480,8 @@
         </div>
         <canvas id="cardCanvas" width="720" height="1500" style="display:none"></canvas>
         <div class="share-row">
-          <button class="btn-share share" id="shareBtn">${t('share')}</button>
           <button class="btn-share save" id="saveBtn">${t('save_image')}</button>
+          <button class="btn-share share" id="shareBtn">${t('share')}</button>
         </div>
         ` : `
         <div class="share-row">
